73c92510d648c7e6871fbe07100c737ef8eaa7e8
neskoi <57025040+neskoi@users.noreply.github.com>
2021-03-26 20:20:59 -0300

    Fix body parser (#1)
    * feat: adicao do banco de dados.
    
    * fix: wrong upload.
    
    * fix: configuracao do body-parser

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const http = require('http');
+const bodyParser = require('body-parser')
 const routes = require('./routes/index');
 require('dotenv');
 
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 4100;
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 
 server.listen(port, () => {
