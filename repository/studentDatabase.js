613f29cb5abc4e0348a731950b3f7e6cf8af73f2
neskoi <57025040+neskoi@users.noreply.github.com>
2021-04-03 11:44:48 -0300

    Dev (#20)
    * implementado crud
    
    * feat: controllers de person e user
    
    * feat: Rotas feitas
    
    * feat: validaçao cpf, rota de person
    
    * feat login
    
    * feat rotas finalizadas
    
    * controllers finalizados
    
    * feat: updateStudent e avatar
    
    * feat: school endpoints
    
    * Feat pagamento (#12)
    
    * feat: rota de inserção de pagamento funcionando.
    
    * feat: conclusão das rotas de pagamento.
    
    * fix: ponto duplicado no routes/index.js, troca de raw query para medoto knex em ./repository/studentDatabase.js
    
    * feat: getStudent (All/ById)
    
    * fix: bloqueando rotas que necessitam de autenticação, refatorando get dos alunos do banco. (#17)
    
    * fix: studentRoutes
    
    Co-authored-by: Karla Thamiris <karlatn@outlook.com>
    Co-authored-by: milena <milena.costandrade123456@gmail.com>
    Co-authored-by: rafae <rafaelgf@gmail.com>
    Co-authored-by: Rafael Gomes da Fontes <71147794+rafagf77@users.noreply.github.com>
    Co-authored-by: milena-costa <81273891+milena-costa@users.noreply.github.com>

--- repository/studentDatabase.js
+++ repository/studentDatabase.js
@@ -2,12 +2,22 @@ const db = require("../repository/db");
 const TABLE_STUDENT = 'filho';
 const TABLE_SCHOOL = 'escola';
 const TABLE_STATE = 'estado';
+const TABLE_USER = 'usuario';
+const TABLE_PERSON = 'pessoa';
 
 class studentDatabase {
   constructor(connection) {
     this.db = connection;
   }
-  async postStudent(person_id, school, name, cpf, img_avatar_url, year, description) {
+  async postStudent(user, school, name, cpf, img_avatar_url, year, description) {
+     const person = await db.raw(`
+        SELECT filho.fk_pessoa FROM ${TABLE_STUDENT} filho
+        LEFT JOIN ${TABLE_SCHOOL} escola ON escola.pk_escola = filho.fk_escola
+        LEFT JOIN ${TABLE_PERSON} pessoa ON pessoa.pk_pessoa = filho.fk_pessoa
+        LEFT JOIN ${TABLE_USER} usuario ON usuario.pk_usuario = pessoa.fk_usuario
+        WHERE usuario.email="${user}";
+     `) 
+     const person_id = person[0][0].fk_pessoa
      await db(TABLE_STUDENT).insert({fk_pessoa:person_id, fk_escola: school, nome: name, cpf: cpf, img_avatar_url: img_avatar_url, ano_escolar: year, historia: description});
   }
 
@@ -15,11 +25,14 @@ class studentDatabase {
     await db(TABLE_STUDENT).where({pk_filho: student_id}).update({fk_escola: school, img_avatar_url: img_avatar_url, ano_escolar: year, historia: description})
   }
 
-  async getAll() {
+  async getAllByPerson(email) {
 
     const result = await db.raw(`
-        SELECT pk_filho, fk_pessoa, ${TABLE_STUDENT}.nome as nome, cpf, img_avatar_url, ${TABLE_SCHOOL}.nome as escola, ano_escolar, historia FROM ${TABLE_STUDENT} filho
-        LEFT JOIN ${TABLE_SCHOOL} escola ON escola.pk_escola = filho.fk_escola;
+        SELECT pk_filho, fk_pessoa, ${TABLE_STUDENT}.nome as nome, filho.cpf as cpf, img_avatar_url, ${TABLE_SCHOOL}.nome_escola as escola, ano_escolar, historia FROM ${TABLE_STUDENT} filho
+        LEFT JOIN ${TABLE_SCHOOL} escola ON escola.pk_escola = filho.fk_escola
+        LEFT JOIN ${TABLE_PERSON} pessoa ON pessoa.pk_pessoa = filho.fk_pessoa
+        LEFT JOIN ${TABLE_USER} usuario ON usuario.pk_usuario = pessoa.fk_usuario
+        WHERE usuario.email="${email}";
     `);
 
     return result[0]
@@ -28,7 +41,7 @@ class studentDatabase {
   async getById(id) {
 
     const result = await db.raw(`
-        SELECT pk_filho, fk_pessoa, ${TABLE_STUDENT}.nome as nome, cpf, img_avatar_url, ${TABLE_SCHOOL}.nome as escola, ano_escolar, ${TABLE_STATE}.nome as estado, historia FROM db_back.filho filho
+        SELECT pk_filho, fk_pessoa, ${TABLE_STUDENT}.nome as nome, cpf, img_avatar_url, ${TABLE_SCHOOL}.nome_escola as escola, ano_escolar, ${TABLE_STATE}.nome as estado, historia FROM db_back.filho filho
         LEFT JOIN ${TABLE_SCHOOL} escola ON escola.pk_escola = filho.fk_escola
         LEFT JOIN ${TABLE_STATE} estado ON estado.pk_estado = escola.fk_estado
         WHERE pk_filho=${id};
