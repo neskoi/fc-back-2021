5a9b83a16dfcc0b199dc75f1baaccc1fa4ad6f2c
neskoi <57025040+neskoi@users.noreply.github.com>
2021-04-04 19:00:05 -0300

    Dev (#23)
    * implementado crud
    
    * feat: controllers de person e user
    
    * feat: Rotas feitas
    
    * feat: validaçao cpf, rota de person
    
    * feat login
    
    * feat rotas finalizadas
    
    * controllers finalizados
    
    * feat: updateStudent e avatar
    
    * feat: school endpoints
    
    * Feat pagamento (#12)
    
    * feat: rota de inserção de pagamento funcionando.
    
    * feat: conclusão das rotas de pagamento.
    
    * fix: ponto duplicado no routes/index.js, troca de raw query para medoto knex em ./repository/studentDatabase.js
    
    * feat: getStudent (All/ById)
    
    * fix: bloqueando rotas que necessitam de autenticação, refatorando get dos alunos do banco. (#17)
    
    * fix: studentRoutes
    
    * Crud karla milena (#21)
    
    * feat- Melhorias na rota e Início documentação
    
    * melhorias no cadastro
    
    * fix: reduzindo carga da query de escolas. (#22)
    
    Co-authored-by: Karla Thamiris <karlatn@outlook.com>
    Co-authored-by: milena <milena.costandrade123456@gmail.com>
    Co-authored-by: rafae <rafaelgf@gmail.com>
    Co-authored-by: Rafael Gomes da Fontes <71147794+rafagf77@users.noreply.github.com>
    Co-authored-by: milena-costa <81273891+milena-costa@users.noreply.github.com>

--- controllers/personController.js
+++ controllers/personController.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+const { promisify } = require('util')
 const knexfile = require ('../repository/db.js')
 const select = knexfile('pessoa')
 const validaCPF = require('../utils/validaCPF')
@@ -15,30 +17,43 @@ class personController{
     }
   }
 
-  async cadastrarPessoa(dataPessoa) {
+  async cadastrarPessoa(req, res) {
     try{
-      console.log('CPF válido?', validaCPF(dataPessoa.cpf))
+      const dataPessoa = req.body
+      const { authorization } = req.headers
+      console.log('authorization', authorization);
+      const [, token] = authorization.split(' ')
+      console.log('token', token);
+      const decoded = await promisify(jwt.verify)(token, process.env.APP_JWT_SECRET)
+      console.log('decoded', decoded);
+      const pk_usuario = decoded.pk_usuario
+      console.log('pk_usuario', pk_usuario);
       if(validaCPF(dataPessoa.cpf)){
         const insert = await knexfile('pessoa').insert({
           fk_estado: dataPessoa.fk_estado,
           fk_banco: dataPessoa.fk_banco,
-          fk_usuario: dataPessoa.fk_usuario,
+          fk_usuario: pk_usuario,
           nome: dataPessoa.nome,
           cpf: dataPessoa.cpf,
           rg: dataPessoa.rg,
           agencia: dataPessoa.agencia,
           conta: dataPessoa.conta,
         })
-        console.log('Pessoa cadastrada com sucesso', insert)
-        return true
-      } else {
-        console.log('CPF Inválido!')
-        return false;
+        console.log('insert', insert);
+        return res.status(200).json({
+          message: "Usuário cadastrado com sucesso!"
+        }) 
+       
       }
-    } catch (e) {
-      console.log('ERRO', e.message)
-      return false
+      }  catch (e) {
+      return res.status(400).json({
+        message: "Não foi possível cadastrar o usuário: " + e.message
+        })
+      }
     }
-  }
-}
+
+    }
+
+ 
+
 module.exports = new personController()
