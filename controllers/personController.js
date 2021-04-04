a3aa1b448757e638a37d8897ab8cd47a0803c39b
neskoi <57025040+neskoi@users.noreply.github.com>
2021-04-04 19:49:20 -0300

    Fix query (#26)
    * fix: reduzindo carga da query de escolas.
    
    * fix: redução dos logs.
    
    * fix: query com retorno undefined corrigida(postStudent)

--- controllers/personController.js
+++ controllers/personController.js
@@ -20,7 +20,6 @@ class personController{
     try{
       const dataPessoa = req.body
       const { authorization } = req.headers
-
       const [, token] = authorization.split(' ')
       const decoded = await promisify(jwt.verify)(token, process.env.APP_JWT_SECRET)
       const pk_usuario = decoded.pk_usuario
